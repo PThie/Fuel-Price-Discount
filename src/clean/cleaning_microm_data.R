87c9ced49e3959f9be70c25ccad0ef481bd8337b
Patrick Thiel <patrickthiel@outlook.de>
2024-04-20 15:22:19 +0200

    Population shares for different age groups
    Added the population shares of different age groups  to the grid data as this is needed for calculating the affected population.

--- src/clean/cleaning_microm_data.R
+++ src/clean/cleaning_microm_data.R
@@ -18,7 +18,20 @@ cleaning_microm_data <- function(
 
     griddata_prep <- microm_data_raw |>
         dplyr::filter(year == 2020) |>
-        dplyr::select(r1_id, r1_mpi_w_dichte, r1_kkr_w_summe, r1_ewa_a_gesamt) |>
+        dplyr::select(
+            r1_id, r1_mpi_w_dichte, r1_kkr_w_summe, r1_ewa_a_gesamt,
+            r1_eag_p_m15bis18, r1_eag_p_w15bis18,
+            r1_eag_p_m18bis20, r1_eag_p_w18bis20,
+            r1_eag_p_m20bis25, r1_eag_p_w20bis25, 
+            r1_eag_p_m25bis30, r1_eag_p_w25bis30, 
+            r1_eag_p_m30bis35, r1_eag_p_w30bis35,
+            r1_eag_p_m35bis40, r1_eag_p_w35bis40,
+            r1_eag_p_m40bis45, r1_eag_p_w40bis45,
+            r1_eag_p_m45bis50, r1_eag_p_w45bis50,
+            r1_eag_p_m50bis55, r1_eag_p_w50bis55,
+            r1_eag_p_m55bis60, r1_eag_p_w55bis60, 
+            r1_eag_p_m60bis65, r1_eag_p_w60bis65
+        ) |>
         as.data.frame()
 
     # join municipality info
