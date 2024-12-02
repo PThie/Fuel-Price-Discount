ac343107231378cd3cb0cb73723dde63d3e8ec17
Patrick Thiel <patrickthiel@outlook.de>
2024-12-02 14:15:53 +0100

    Plot HonestDiD dates in one figure
    Adjusted the code such that the three HonestDiD dates are plotted in one figure
    (by fuel type).

--- src/plot/plotting_robust_trends_dayspecific.R
+++ src/plot/plotting_robust_trends_dayspecific.R
@@ -16,71 +16,78 @@ plotting_robust_trends_dayspecific <- function(
     # define fuel types
     dep_cases <- c("diesel", "e10")
 
-    # loop through honestDiD results
-    for (dta in honest_did_days) {
-        # loop through fuel types
-        for(dep_case in dep_cases) {
-            # subset data
-            moddata <- dta |>
-                dplyr::filter(fuel_type == dep_case)
+    # combine all data
+    dta <- data.table::rbindlist(honest_did_days)
 
-            period_label <- unique(moddata$period_label)
+    # loop through fuel types
+    for(dep_case in dep_cases) {
+        # subset data
+        moddata <- dta |>
+            dplyr::filter(fuel_type == dep_case)
 
-            # generate plot
-            vio_plot <- ggplot(
-                data = moddata,
-                aes(
-                    x = mbar_labels,
-                    y = ub
+        # generate plot
+        vio_plot <- ggplot(
+            data = moddata,
+            aes(
+                x = mbar_labels,
+                y = ub,
+                group = period_label,
+                col = period_label
+            ),
+            col = "black"
+        )+
+            geom_errorbar(
+                mapping = aes(
+                    ymin = lb,
+                    ymax = ub
                 ),
-                col = "black"
+                width = 0.2,
+                size = 1.3,
+                position = position_dodge()
             )+
-                geom_errorbar(
-                    mapping = aes(
-                        ymin = lb,
-                        ymax = ub
-                    ),
-                    width = 0.15,
-                    size = 1
-                )+
-                geom_hline(
-                    yintercept = 0
-                )+
-                labs(
-                    x = "M",
-                    y = ""
-                )+
-                theme_classic()+
-                theme(
-                    panel.border = element_rect(linewidth = 1, fill = NA),
-                    axis.text = element_text(size = 19),
-                    axis.title = element_text(size = 20),
-                    legend.key.size = unit(1, "cm"),
-                    legend.title = element_text(size = 20),
-                    legend.text = element_text(size = 20),
-                    legend.position = "bottom"
-                )+
-                guides(col = guide_legend(nrow = 4, byrow = TRUE))
-            
-            # export
-            filename <- paste0(
-                "honestdid_plot_one_day_",
-                dep_case,
-                "_",
-                period_label,
-                ".png"
-            )
-            suppressMessages(ggsave(
-                plot = vio_plot,
-                file.path(
-                    config_paths()[["output_path"]],
-                    "graphs",
-                    filename
+            geom_hline(
+                yintercept = 0
+            )+
+            scale_color_manual(
+                values = c(
+                    "2022-06-01" = config_globals()[["java_five_colors"]][1],
+                    "2022-07-01" = config_globals()[["java_five_colors"]][3],
+                    "2022-08-01" = config_globals()[["java_five_colors"]][5]
                 ),
-                height = 8,
-                width = 10
-            ))
-        }
+                name = ""
+            )+
+            labs(
+                x = "M",
+                y = ""
+            )+
+            theme_classic()+
+            theme(
+                panel.border = element_rect(linewidth = 1, fill = NA),
+                axis.text = element_text(size = 19),
+                axis.title = element_text(size = 20),
+                legend.key.size = unit(1, "cm"),
+                legend.title = element_text(size = 20),
+                legend.text = element_text(size = 20),
+                legend.position = "bottom"
+            )+
+            guides(col = guide_legend(nrow = 1, ncol = 3, byrow = TRUE))
+        
+        # export
+        filename <- paste0(
+            "honestdid_plot_days_",
+            dep_case,
+            ".png"
+        )
+        suppressMessages(ggsave(
+            plot = vio_plot,
+            file.path(
+                config_paths()[["output_path"]],
+                "graphs",
+                filename
+            ),
+            height = 8,
+            width = 10
+        ))
     }
 
     #--------------------------------------------------
---
+++ src/plot/plotting_robust_trends_dayspecific_states.R
@@ -0,0 +1,106 @@
+plotting_robust_trends_dayspecific_states <- function(
+    honest_did_days = NA
+) {
+    #' @title Plotting HonestDiD Results for specific days and states
+    #' 
+    #' @description This function plots the HonestDiD results for specific days
+    #' and states.
+    #' 
+    #' @param honest_did_days HonestDiD results
+    #'
+    #' @return NULL, graphs 
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # loop through options
+    
+    # define fuel types
+    dep_cases <- c("diesel", "e10")
+
+    # loop through honestDiD results
+    for (state in names(honest_did_days)) {
+        # subset for state
+        dta <- honest_did_days[[state]]
+
+        # combine all state data
+        dta <- data.table::rbindlist(dta)
+
+        # loop through fuel types
+        for (dep_case in dep_cases) {
+            # subset for fuel type
+            moddata <- dta |>
+                dplyr::filter(fuel_type == dep_case)
+
+            # generate plot
+            vio_plot <- ggplot(
+                data = moddata,
+                aes(
+                    x = mbar_labels,
+                    y = ub,
+                    group = period_label,
+                    col = period_label
+                ),
+                col = "black"
+            )+
+                geom_errorbar(
+                    mapping = aes(
+                        ymin = lb,
+                        ymax = ub
+                    ),
+                    width = 0.2,
+                    size = 1.3,
+                    position = position_dodge()
+                )+
+                geom_hline(
+                    yintercept = 0
+                )+
+                scale_color_manual(
+                    values = c(
+                        "2022-06-01" = config_globals()[["java_five_colors"]][1],
+                        "2022-07-01" = config_globals()[["java_five_colors"]][3],
+                        "2022-08-01" = config_globals()[["java_five_colors"]][5]
+                    ),
+                    name = ""
+                )+
+                labs(
+                    x = "M",
+                    y = ""
+                )+
+                theme_classic()+
+                theme(
+                    panel.border = element_rect(linewidth = 1, fill = NA),
+                    axis.text = element_text(size = 19),
+                    axis.title = element_text(size = 20),
+                    legend.key.size = unit(1, "cm"),
+                    legend.title = element_text(size = 20),
+                    legend.text = element_text(size = 20),
+                    legend.position = "bottom"
+                )+
+                guides(col = guide_legend(nrow = 1, ncol = 3, byrow = TRUE))
+
+            # export
+            filename <- paste0(
+                "honestdid_plot_days_",
+                dep_case,
+                "_",
+                state,
+                ".png"
+            )
+            suppressMessages(ggsave(
+                plot = vio_plot,
+                file.path(
+                    config_paths()[["output_path"]],
+                    "graphs",
+                    filename
+                ),
+                height = 8,
+                width = 10
+            ))
+        }
+    }
+
+    #--------------------------------------------------
+    # return
+
+    return(NULL)
+}
