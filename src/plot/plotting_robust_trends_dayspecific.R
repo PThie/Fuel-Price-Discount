5724bdd14aaef26feb5f3213f450e99c047871ec
Patrick Thiel <patrickthiel@outlook.de>
2025-05-16 08:51:35 +0200

    Reworked the figure design

--- src/plot/plotting_robust_trends_dayspecific.R
+++ src/plot/plotting_robust_trends_dayspecific.R
@@ -16,78 +16,71 @@ plotting_robust_trends_dayspecific <- function(
     # define fuel types
     dep_cases <- c("diesel", "e10")
 
-    # combine all data
-    dta <- data.table::rbindlist(honest_did_days)
+    # loop through honestDiD results
+    for (dta in honest_did_days) {
+        # loop through fuel types
+        for(dep_case in dep_cases) {
+            # subset data
+            moddata <- dta |>
+                dplyr::filter(fuel_type == dep_case)
 
-    # loop through fuel types
-    for(dep_case in dep_cases) {
-        # subset data
-        moddata <- dta |>
-            dplyr::filter(fuel_type == dep_case)
+            period_label <- unique(moddata$period_label)
 
-        # generate plot
-        vio_plot <- ggplot(
-            data = moddata,
-            aes(
-                x = mbar_labels,
-                y = ub,
-                group = period_label,
-                col = period_label
-            ),
-            col = "black"
-        )+
-            geom_errorbar(
-                mapping = aes(
-                    ymin = lb,
-                    ymax = ub
+            # generate plot
+            vio_plot <- ggplot(
+                data = moddata,
+                aes(
+                    x = mbar_labels,
+                    y = ub
                 ),
-                width = 0.2,
-                size = 1.3,
-                position = position_dodge()
+                col = "black"
             )+
-            geom_hline(
-                yintercept = 0
-            )+
-            scale_color_manual(
-                values = c(
-                    "2022-06-01" = config_globals()[["java_five_colors"]][1],
-                    "2022-07-01" = config_globals()[["java_five_colors"]][3],
-                    "2022-08-01" = config_globals()[["java_five_colors"]][5]
+                geom_errorbar(
+                    mapping = aes(
+                        ymin = lb,
+                        ymax = ub
+                    ),
+                    width = 0.2,
+                    size = 1.3
+                )+
+                geom_hline(
+                    yintercept = 0
+                )+
+                labs(
+                    x = "M",
+                    y = ""
+                )+
+                theme_classic()+
+                theme(
+                    panel.border = element_rect(linewidth = 1, fill = NA),
+                    axis.text = element_text(size = 19),
+                    axis.title = element_text(size = 20),
+                    legend.key.size = unit(1, "cm"),
+                    legend.title = element_text(size = 20),
+                    legend.text = element_text(size = 20),
+                    legend.position = "bottom"
+                )+
+                guides(col = guide_legend(nrow = 4, byrow = TRUE))
+            
+            # export
+            filename <- paste0(
+                "honestdid_plot_days_",
+                dep_case,
+                "_",
+                period_label,
+                ".png"
+            )
+            suppressMessages(ggsave(
+                plot = vio_plot,
+                file.path(
+                    config_paths()[["output_path"]],
+                    "graphs",
+                    filename
                 ),
-                name = ""
-            )+
-            labs(
-                x = "M",
-                y = ""
-            )+
-            theme_classic()+
-            theme(
-                panel.border = element_rect(linewidth = 1, fill = NA),
-                axis.text = element_text(size = 19),
-                axis.title = element_text(size = 20),
-                legend.key.size = unit(1, "cm"),
-                legend.title = element_text(size = 20),
-                legend.text = element_text(size = 20),
-                legend.position = "bottom"
-            )+
-            guides(col = guide_legend(nrow = 1, ncol = 3, byrow = TRUE))
-        
-        # export
-        filename <- paste0(
-            "honestdid_plot_days_",
-            dep_case,
-            ".png"
-        )
-        suppressMessages(ggsave(
-            plot = vio_plot,
-            file.path(
-                config_paths()[["output_path"]],
-                "graphs",
-                filename
-            ),
-            height = 8,
-            width = 10
-        ))
+                height = 8,
+                width = 10
+            ))
+        }
     }
 
     #--------------------------------------------------
