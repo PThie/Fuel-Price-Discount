8391fc306480120786e6293bf9c6c1488d6c1f03
Patrick Thiel <patrickthiel@outlook.de>
2025-05-19 09:39:11 +0200

    Create subsetting_french_fuel_prices.R
    Function to prepare the fuel price data for the analysis of the French FTD.

---
+++ src/subset/subsetting_french_fuel_prices.R
@@ -0,0 +1,40 @@
+subsetting_french_fuel_prices <- function(
+    price_data = NA
+) {
+    #' @title Preparing French fuel prices
+    #' 
+    #' @description This function prepares the fuel price data for France
+    #' for the analysis of the fuel price discount.
+    #' 
+    #' @param price_data Fuel price data
+    #' 
+    #' @return Returns a data frame with the prepared fuel price data
+    #' @author Patrick Thiel
+
+    #--------------------------------------------------
+    # drop columns related to German FTD
+
+    price_data <- price_data |>
+        dplyr::select(-dplyr::starts_with("treat"))
+
+    #--------------------------------------------------
+    # define dummies for treatment group (France) and treatment period (after
+    # April 2022)
+
+    price_data <- price_data |>
+        dplyr::mutate(
+            treat_region_fr = dplyr::case_when(
+                country == "FR" ~ 1,
+                country != "FR" ~ 0
+            ),
+            treat_ftd_fr = dplyr::case_when(
+                date >= "2022-04-01" ~ 1,
+                date < "2022-04-01" ~ 0
+            )
+        )
+
+    #--------------------------------------------------
+    # return
+
+    return(price_data)
+}
