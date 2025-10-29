bc270f8c060eed199267283bcf53c981275799b9
Patrick Thiel <patrickthiel@outlook.de>
2025-10-29 10:01:41 +0100

    Generalized reading_french_regions to allow reading more units

--- src/read/reading_french_regions.R
+++ src/read/reading_french_regions.R
@@ -1,11 +1,13 @@
 reading_french_regions <- function(
-    data_file_path = NA
+    data_file_path = NA,
+    region_name_id = NA
 ) {
     #' @title Reading French regions
     #' 
     #' @description This function reads the French regions from the shapefile.
     #' 
     #' @param data_file_path Path to the shapefile
+    #' @param region_name_id Name of the regional level
     #' 
     #' @return Dataframe with French regions
     #' @author Patrick Thiel
@@ -16,14 +18,26 @@ reading_french_regions <- function(
     regions <- sf::st_read(
         data_file_path,
         quiet = TRUE
-    )
+    ) |>
+    sf::st_transform(config_globals()[["utmcrs"]])
 
     #--------------------------------------------------
     # clean regions
 
+    if (region_name_id == "region") {
+        regions <- regions |>
+            dplyr::select(running_id_region = ID_1, region_name = NAME_1)
+    } else if (region_name_id == "department") {
+        regions <- regions |>
+            dplyr::select(running_id_region = code_insee, region_name = nom)
+    } else {
+        regions <- regions |>
+            dplyr::select(running_id_region = insee, region_name = nom)
+    }
+
+    # adjust names
     regions <- regions |>
-        dplyr::select(running_id_region = ID_1, region_name = NAME_1) |>
-        dplyr:::mutate(
+        dplyr::mutate(
             region_name = stringi::stri_trans_general(
                 region_name,
                 "de-ASCII; Latin-ASCII"
